ec2dabdb20fc20c96b6823b13366db31d930b509
Igor Túllio <igor.goncalves@basis.com.br>
2018-06-28 11:46:54 -0300

    Adição de comentários

--- velha.py
+++ velha.py
@@ -1,13 +1,12 @@
-import random
+import random  # importação da classe random para realizar sorteios
 
-campo = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
-jogadorPeca = ""
-jogador = 0
-oponentePeca = ""
-oponente = 0
+campo = ["_", "_", "_", "_", "_", "_", "_", "_", "_"] # variável global do campo
+jogadorPeca = ""  # variável global da peça do jogador
+jogador = random.choice((0, 1))  # variável global referente a vez da jogada
+oponentePeca = ""  # variável global da peça da computador
 
 
-def imprimeCampo(campo):
+def imprimeCampo(campo):  # método para imprimir o campo de jogo
     c = 0
     for i in campo:
         if c % 3 == 0:
@@ -19,7 +18,7 @@ def imprimeCampo(campo):
     print("\n")
 
 
-def ganhou(simbolo, campo):
+def ganhou(simbolo, campo):  # método que verifica vencedor (de acordo com as regras do jogo) após cada jogada
     if campo[0] == simbolo and campo[1] == simbolo and campo[2] == simbolo:
         return 1
     if campo[3] == simbolo and campo[4] == simbolo and campo[5] == simbolo:
@@ -38,19 +37,19 @@ def ganhou(simbolo, campo):
         return 1
 
 
-def velha(campo):
+def velha(campo):  # método que verfica se deu velha
     if '_' not in campo:
         return 1
 
 
-def menu():
+def menu():  # método para o jogador escolher sua peça
     print("Jogo da velha!")
     resposta = input("Você quer ser X ou O? ")
     while resposta != "X" and resposta != "0":
         resposta = input("Resposta inválida! Você quer ser X ou O? ")
 
-    global jogadorPeca
-    global oponentePeca
+    global jogadorPeca  # identifica a variável como global
+    global oponentePeca  # identifica a variável como global
     if resposta == "X":
         jogadorPeca = "X"
         oponentePeca = "O"
@@ -59,24 +58,25 @@ def menu():
         oponentePeca = "X"
 
 
-def __main__():
+def __main__():  # método principal que chama os demais
     menu()
     jogo()
 
 
-def jogo():
-    global jogadorPeca
-    global oponentePeca
-    global jogador
-    while 1:
-        if velha(campo):
+def jogo():  # método que realiza o jogo
+    global jogadorPeca  # identifica a variável como global
+    global oponentePeca  # identifica a variável como global
+    global jogador  # identifica a variável como global
+
+    while 1:  # while infinito
+        if velha(campo):  # verifica se deu velha
             imprimeCampo(campo)
             print("Velha!")
             break
-        if jogador:
+        if jogador:  # seleciona a vez da jogada
             imprimeCampo(campo)
 
-            while 1:
+            while 1:  # jogador seleciona a casa onde deseja jogar
                 entrada = int(input("Digite a casa [0-8]: "))
                 while entrada > 8:
                     entrada = int(input("Entrada inválida! Digite a casa [0-8]: "))
@@ -96,7 +96,7 @@ def jogo():
         else:
             imprimeCampo(campo)
             while 1:
-                pc = random.randint(0, 8)
+                pc = random.randint(0, 8)  # é sorteado uma jogada para o oponente
 
                 if campo[pc] == '_':
                     break
@@ -111,4 +111,4 @@ def jogo():
                 break
 
 
-__main__()
+__main__()  # chama o método para iniciar o programa

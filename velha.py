44f086ce9817e484b7992939ab04fe3f944bd05e
Igor Túllio <igor.goncalves@basis.com.br>
2018-06-28 11:28:36 -0300

    Melhoramento do código

--- velha.py
+++ velha.py
@@ -1,8 +1,10 @@
 import random
 
 campo = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
-
-jogador = random.choice((0, 1))
+jogadorPeca = ""
+jogador = 0
+oponentePeca = ""
+oponente = 0
 
 
 def imprimeCampo(campo):
@@ -16,87 +18,97 @@ def imprimeCampo(campo):
 
     print("\n")
 
+
 def ganhou(simbolo, campo):
     if campo[0] == simbolo and campo[1] == simbolo and campo[2] == simbolo:
         return 1
-
     if campo[3] == simbolo and campo[4] == simbolo and campo[5] == simbolo:
         return 1
-
     if campo[6] == simbolo and campo[7] == simbolo and campo[8] == simbolo:
         return 1
-
     if campo[0] == simbolo and campo[3] == simbolo and campo[6] == simbolo:
         return 1
-
     if campo[1] == simbolo and campo[4] == simbolo and campo[7] == simbolo:
         return 1
-
     if campo[2] == simbolo and campo[5] == simbolo and campo[8] == simbolo:
         return 1
-
     if campo[0] == simbolo and campo[4] == simbolo and campo[8] == simbolo:
         return 1
-
     if campo[2] == simbolo and campo[4] == simbolo and campo[6] == simbolo:
         return 1
+
 
 def velha(campo):
     if '_' not in campo:
         return 1
 
+
 def menu():
     print("Jogo da velha!")
-    resposta = input("Você quer ser X ou O?")
-    while resposta != "X" or "O":
-        if resposta == "X":
-            jogador = "X"
-            oponente = "O"
-        else:
-            jogador = "O"
-            oponente = "X"
-        resposta = input("Você quer ser X ou O?")
+    resposta = input("Você quer ser X ou O? ")
+    while resposta != "X" and resposta != "0":
+        resposta = input("Resposta inválida! Você quer ser X ou O? ")
+
+    global jogadorPeca
+    global oponentePeca
+    if resposta == "X":
+        jogadorPeca = "X"
+        oponentePeca = "O"
+    else:
+        jogadorPeca = "O"
+        oponentePeca = "X"
 
 
 def __main__():
-    pass
+    menu()
+    jogo()
 
-while 1:
-    if velha(campo):
-        imprimeCampo(campo)
-        print("Velha!")
-        break
-    if jogador:
-        imprimeCampo(campo)
 
-        while 1:
-            vc = int(input())
+def jogo():
+    global jogadorPeca
+    global oponentePeca
+    global jogador
+    while 1:
+        if velha(campo):
+            imprimeCampo(campo)
+            print("Velha!")
+            break
+        if jogador:
+            imprimeCampo(campo)
 
-            if campo[vc] == '_':
+            while 1:
+                entrada = int(input("Digite a casa [0-8]: "))
+                while entrada > 8:
+                    entrada = int(input("Entrada inválida! Digite a casa [0-8]: "))
+                entrada
+
+                if campo[entrada] == '_':
+                    break
+
+            campo[entrada] = jogadorPeca
+
+            jogador = 0
+
+            if ganhou(jogadorPeca, campo):
+                imprimeCampo(campo)
+                print("Você ganhou!")
+                break
+        else:
+            imprimeCampo(campo)
+            while 1:
+                pc = random.randint(0, 8)
+
+                if campo[pc] == '_':
+                    break
+
+            campo[pc] = oponentePeca
+
+            jogador = 1
+
+            if ganhou(oponentePeca, campo):
+                imprimeCampo(campo)
+                print("Você perdeu!")
                 break
 
-        campo[vc] = sVC
 
-        jogador = 0
-
-        if ganhou(sVC,campo):
-            imprimeCampo(campo)
-            print("Você ganhou!")
-            break
-
-    else:
-        imprimeCampo(campo)
-        while 1:
-            pc = random.randint(0,8)
-
-            if campo[pc] == '_':
-                break
-
-        campo[pc] = sPC
-
-        jogador = 1
-
-        if ganhou(sPC, campo):
-            imprimeCampo(campo)
-            print("Você perdeu!")
-            break
+__main__()
